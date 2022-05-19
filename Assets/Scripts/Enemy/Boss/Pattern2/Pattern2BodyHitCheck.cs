0b6523a794559cb5454dc8d240d75efc1769261d
Rostreaca <freestalere@naver.com>
2022-05-19 20:01:52 +0900

    2022.05.19
    보스 애니메이션 적용
    일부 패턴에서 발생하는 문제들 해결

--- Assets/Scripts/Enemy/Boss/Pattern2/Pattern2BodyHitCheck.cs
+++ Assets/Scripts/Enemy/Boss/Pattern2/Pattern2BodyHitCheck.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Pattern2BodyHitCheck : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != null && collision.gameObject.tag == "Ground")//플레이어가 트리거를 빠져나갔을 떄가 아닌 보스가 벽에 박았을 때 Pattern2start가 false가 되야함.
+        {
+            BossTest.instance.anim.SetBool("Pattern2start", false);
+            BossTest.instance.anim.SetBool("Pattern2isCool", true);
+        }
 
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject != null && collision.gameObject.tag == "Ground")//플레이어가 트리거를 빠져나갔을 떄가 아닌 보스가 벽에 박았을 때 Pattern2start가 false가 되야함.
