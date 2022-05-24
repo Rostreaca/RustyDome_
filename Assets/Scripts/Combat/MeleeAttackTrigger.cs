4b3c74e33bc3b8541060aeedcc1e1f65ae9feb03
seojung <seo980620@gmail.com>
2022-05-25 00:37:44 +0900

    5.25

--- Assets/Scripts/Combat/MeleeAttackTrigger.cs
+++ Assets/Scripts/Combat/MeleeAttackTrigger.cs
@@ -6,9 +6,6 @@ public class MeleeAttackTrigger : MonoBehaviour
 {
     public Combat combat;
 
-    private void Start()
-    {
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         combat.colliderDetected = collision;
