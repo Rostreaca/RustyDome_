6e402b2f38c99df4e014e8dfd84c2800fd46f850
Rostreaca <freestalere@naver.com>
2022-11-28 16:21:11 +0900

    2022.11.28
    상호작용시 이동하는 포탈 생성

---
+++ Assets/Scripts/Scene/MoveInteractScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveInteractScene : MonoBehaviour
+{
+    [Header("이동할 씬 넘버")]
+    public int PassSceneNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject !=null && collision.gameObject.CompareTag("Player"))
+        {
+            if(Input.GetKey(KeyCode.F))
+            {
+                SceneManager.LoadScene(PassSceneNumber);
+            }
+        }
+    }
+}
