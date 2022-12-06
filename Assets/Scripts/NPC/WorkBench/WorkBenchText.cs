571f21e4a0450ef69cbb0b899ebef4f8dd8b6086
Rostreaca <freestalere@naver.com>
2022-12-06 23:07:26 +0900

    1206

--- Assets/Scripts/NPC/WorkBench/WorkBenchText.cs
+++ Assets/Scripts/NPC/WorkBench/WorkBenchText.cs
@@ -49,10 +49,9 @@ public class WorkBenchText : UIText
     {
         if (WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).IsName("WorkBench1_Open_Ani") || WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).IsName("WorkBench2_Open_Ani") || WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).IsName("WorkBench3_Open_Ani") || WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).IsName("WorkBench4_Open_Ani"))
         {
-
-            if (WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            if (WorkBenchController.instance.anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f)
             {
-                if (PlayerController.instance.animator.GetCurrentAnimatorStateInfo(0).IsName("char_analyze_start") && PlayerController.instance.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                if (PlayerController.instance.animator.GetCurrentAnimatorStateInfo(0).IsName("char_analyze_start") && PlayerController.instance.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f)
                 {
                     UIManager.instance.ChangeScreen(UIManager.ScreenState.Customize);
                     Customize.instance.canCustomize = true;
